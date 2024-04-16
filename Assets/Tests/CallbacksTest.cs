b5b71f48e3276d894f9286b05f8a9d7b0de51ac8
DESKTOP-5QT8APO\Vasyl Khreptak <VasylKhreptak12@gmail.com>
2024-04-16 10:55:56 +0300

    - updated CallbacksTest;

--- Assets/Tests/CallbacksTest.cs
+++ Assets/Tests/CallbacksTest.cs
@@ -5,20 +5,13 @@ using Zenject;
 
 namespace Tests
 {
-    public class CallbacksTest : IInitializable, IEnableEventHandler, IDisableEventHandler, ITickable, IFixedTickable, ILateTickable,
-        IDisposable
+    public class CallbacksTest : IInitializable, IEnableEventHandler, IDisableEventHandler, IDisposable
     {
         public void Initialize() => Debug.Log("Initialize");
 
         public void OnEnable() => Debug.Log("OnEnable");
 
         public void OnDisable() => Debug.Log("OnDisable");
-
-        public void Tick() => Debug.Log("Tick");
-
-        public void FixedTick() => Debug.Log("FixedTick");
-
-        public void LateTick() => Debug.Log("LateTick");
 
         public void Dispose() => Debug.Log("Dispose");
     }
